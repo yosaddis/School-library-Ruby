d4f6c200081551331517f6485dcfa3b2a6ce3ace
cosmosha <cosmoshagan@gmail.com>
2023-06-16 15:07:31 +0000

    fix linter errors

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,17 +1,17 @@
 require './student'
 
 describe Student do
+  student = Student.new 24, 'Yosi', 1
+  it 'Creates a new student a kind of person' do
+    expect(student).to be_a_kind_of Person
+  end
+
+  it 'Creates a new student instance of student' do
     student = Student.new 24, 'Yosi', 1
-        it 'Creates a new student a kind of person' do            
-            expect(student).to be_a_kind_of Person
-        end
+    expect(student).to be_an_instance_of Student
+  end
 
-        it 'Creates a new student instance of student' do
-            student = Student.new 24, 'Yosi', 1
-            expect(student).to be_an_instance_of Student
-        end
-
-        it 'Should play hocky' do
-            expect(student.play_hooky).to eq('¯\(ツ)/¯')
-        end
+  it 'Should play hocky' do
+    expect(student.play_hooky).to eq('¯\(ツ)/¯')
+  end
 end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,17 +1,17 @@
 require './teacher'
 
 describe Teacher do
-    teacher = Teacher.new 24, 'Developer', 'Yosi'
-    it 'Creates a new teacher a kind of person' do
-        expect(teacher).to be_a_kind_of Person
-    end
+  teacher = Teacher.new 24, 'Developer', 'Yosi'
+  it 'Creates a new teacher a kind of person' do
+    expect(teacher).to be_a_kind_of Person
+  end
 
-    it 'Creates a new teacher instance of teacher' do
-        teacher = Teacher.new 24, 'Yosi', 'Developer'
-        expect(teacher).to be_an_instance_of Teacher
-    end
+  it 'Creates a new teacher instance of teacher' do
+    teacher = Teacher.new 24, 'Yosi', 'Developer'
+    expect(teacher).to be_an_instance_of Teacher
+  end
 
-    it 'Should be able to use services' do
-        expect(teacher.can_use_services?).to eq(true)
-    end
+  it 'Should be able to use services' do
+    expect(teacher.can_use_services?).to eq(true)
+  end
 end
