d94bb30cede021525e3e714f1ea627cc282c604e
cosmosha <cosmoshagan@gmail.com>
2023-06-16 14:56:12 +0000

    Add capitalize_decoreator_spec file

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,13 @@
+require './capitalize_decorator'
+require './nameable'
+describe CapitalizeDecorator do
+  nameable = Nameable.new
+  capitalize_decorator = CapitalizeDecorator.new nameable
+  it 'Should create an instance of CapitalizeDecorator' do
+    expect(capitalize_decorator).to be_instance_of(CapitalizeDecorator)
+  end
+
+  it 'Should create be kind  of Nameable' do
+    expect(capitalize_decorator).to be_kind_of(BaseDecorator)
+  end
+end
